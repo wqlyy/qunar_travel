8d97812a21e458bd5274f22b425862193d25d94c
wangqiang <wangqiangweb@163.com>
2019-05-07 16:05:57 +0800

    详情头部导航动画

---
+++ src/pages/Detail/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="detail-header">
+    <router-link v-show="showAbs" to="/" tag="div" class="detail-header-abs">
+      <i class="iconfont">&#xe624;</i>
+    </router-link>
+    <div :style="opacityStyle" v-show="!showAbs" class="detail-header-fixed">
+      <router-link to="/" tag="i" class="iconfont">&#xe624;</router-link>
+      景点详情
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "detail-header",
+    data() {
+      return {
+        showAbs: true,
+        opacityStyle: {
+          opacity: 0
+        }
+      }
+    },
+    methods: {
+      handleScroll() {
+        let top = document.documentElement.scrollTop
+        if (top > 60) {
+          let opacity = top / 140
+          opacity = opacity > 1 ? 1 : opacity
+          this.opacityStyle = {
+            opacity
+          }
+          this.showAbs = false
+        } else {
+          this.showAbs = true
+        }
+      }
+    },
+    activated() {
+      window.addEventListener('scroll', this.handleScroll)
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "../../../assets/styles/varibles";
+
+  .detail-header {
+    &-abs {
+      position: absolute;
+      left: .1rem;
+      top: .1rem;
+      width: .8rem;
+      height: .8rem;
+      line-height: .8rem;
+      text-align: center;
+      border-radius: .4rem;
+      background-color: rgba(0, 0, 0, .8);
+      i {
+        color: #fff;
+        font-size: .4rem;
+      }
+    }
+    &-fixed {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: $headerHeight;
+      line-height: $headerHeight;
+      text-align: center;
+      color: #fff;
+      background-color: $bgColor;
+      font-size: .32rem;
+      i {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: .64rem;
+        text-align: center;
+        font-size: .4rem;
+        color: #fff;
+      }
+    }
+  }
+</style>
--- src/pages/Detail/index.vue
+++ src/pages/Detail/index.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <detail-banner/>
+    <detail-header/>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
   import DetailBanner from './components/Banner'
+  import DetailHeader from './components/Header'
   export default {
     name: "index",
     components: {
-      DetailBanner
+      DetailBanner,
+      DetailHeader
     },
     mounted() {
       console.log(this.$route.params.id)
@@ -18,5 +22,7 @@
 </script>
 
 <style scoped lang="scss">
-
+.content{
+  height: 50rem;
+}
 </style>
